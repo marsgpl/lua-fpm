f0d1afd0db323b4c2a6b3c87b8c1faaa73e72217
airevent <marsgpl@gmail.com>
2016-01-04 00:03:40 +0300

    tests: prove of nonblocking execution

--- etc/test.lua
+++ etc/test.lua
@@ -1,6 +1,7 @@
 --
 
 local trace = require "trace"
+local net = require "net"
 
 trace.use_colors = false
 
@@ -9,5 +10,22 @@ trace.use_colors = false
 -- request.stdin
 
 return function( request )
+    print "before yield"
+
+    local s = net.ip4.tcp.socket(1)
+    s:connect(net.ip4.tcp.nslookup("fuck.world")[1], 80)
+    request.client.worker.sockets[s:fd()] = {
+        sock = s,
+        e_onwrite = function()
+            request.client.worker.sockets[s:fd()] = nil
+            s:close()
+            coroutine.resume(request.t)
+        end
+    }
+    request.client.worker.epoll:watch(s:fd(), net.f.EPOLLET | net.f.EPOLLRDHUP | net.f.EPOLLOUT)
+    coroutine.yield()
+
+    print "after yield"
+
     return {}, trace.str(request)
 end
