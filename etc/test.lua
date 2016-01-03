33c06f79d98029b5b2b529b460c075ab39286e20
airevent <marsgpl@gmail.com>
2016-01-03 12:52:33 +0300

    informative test

--- etc/test.lua
+++ etc/test.lua
@@ -1,5 +1,13 @@
 --
 
+local trace = require "trace"
+
+trace.use_colors = false
+
+-- request.params.LUA_PATH
+-- request.params.LUA_ARGS
+-- request.stdin
+
 return function( request )
-    return {}, "hi"
+    return {}, trace.str(request)
 end
